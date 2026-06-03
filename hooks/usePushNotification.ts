8f5a5b2088b68fb55d90f6a9b50459974564c89c
Sloft20 <thiagoelson10@gmail.com>
2026-06-03 00:41:03 -0300

    Add Web Push

--- hooks/usePushNotification.ts
+++ hooks/usePushNotification.ts
@@ -47,7 +47,7 @@ export function usePushNotification(operatorId: string | null) {
       // Criar subscription
       const subscription = await reg.pushManager.subscribe({
         userVisibleOnly:      true,
-        applicationServerKey: urlBase64ToUint8Array(VAPID_PUBLIC_KEY),
+        applicationServerKey: urlBase64ToUint8Array(VAPID_PUBLIC_KEY) as any,
       });
 
       const subJson = subscription.toJSON();
